b2aff7e464f7192333bda5b687600f5d723036ca
ehance <ehance@hydrofarm.com>
2011-12-16 17:40:43 -0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FirstApp::Application.config.secret_token = 'd354cd24928fe6517effd98e13b12d9ceef0bcd41d47e7b5af9191cdd2dbd8f0c292b173ab6c2025acdfafee8d7f15a1ebb07ee687ed2002a2337bc4962174b8'
